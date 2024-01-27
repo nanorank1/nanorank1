8861a4be1461778b5141047dbc9e699ebb5e006e
nanorank1 <157456156+nanorank1@users.noreply.github.com>
2024-01-27 14:19:09 +0700

    Update gg.lua

--- gg.lua
+++ gg.lua
@@ -1,1 +1,18 @@
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+local Window = OrionLib:MakeWindow({Name = "Title of the library", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
 
+local Tab = Window:MakeTab({
+	Name = "Tab 1",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+
+
+Tab:AddButton({
+	Name = "Button!",
+	Callback = function()
+      		print("button pressed")
+  	end    
+})
+
+OrionLib:Destroy()
